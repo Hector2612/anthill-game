4e05c0220dff7a0660df1816d997dd0263b94f8c
Hector2612 <garciapehector@gmail.com>
2025-02-17 10:13:52 +0100

    Update space.h

--- seed-src/space.h
+++ seed-src/space.h
@@ -17,7 +17,7 @@ typedef struct _Space Space;
 
 /**
  * @brief It creates a new space, allocating memory and initializing its members
- * @author Héctor García Pérez
+ * @author Jaime Luna Lavela
  *
  * @param id the identification number for the new space
  * @return a new space, initialized
@@ -140,7 +140,7 @@ Id space_get_west(Space *space);
 
 /**
  * @brief It sets whether the space has an object or not
- * @author Héctor García Pérez
+ * @author Jaime Luna Lavela
  *
  * @param space a pointer to the space
  * @param object the id of the object we want to set
@@ -150,7 +150,7 @@ Status space_set_object(Space *space, Id object);
 
 /**
  * @brief It gets whether the space has an object or not
- * @author Héctor García Pérez
+ * @author Jaime Luna Lavela
  *
  * @param space a pointer to the space
  * @return the id of the space's object
